da4899208b988fce4624c2247c808403b5e44764
Ettiene Mare <ettienemare@gmail.com>
2021-06-07 20:35:37 +0200

    work on menu

--- docs/components/menu.js
+++ docs/components/menu.js
@@ -1,6 +1,18 @@
 export class MenuComponent extends HTMLElement {
     async connectedCallback() {
-        this.innerHTML = '<h1>Menu</h1>';
+        this.innerHTML = `
+            <div class='toggle' onclick="this.parentNode.classList.toggle('minimize')">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div class="menu-item"><span class="material-icons">home</span><div>Home</div></div>
+            <div class="menu-item"><span class="material-icons">account_circle</span><div>Account</div></div>
+        `;
+    }
+
+    disconnectedCallback() {
+
     }
 }
 
