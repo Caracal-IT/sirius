74a720e823d3d521db6a45646cd31bafb4a89b0e
Ettiene Mare <ettienemare@gmail.com>
2021-06-08 23:43:19 +0200

    update styles

--- docs/components/menu.js
+++ docs/components/menu.js
@@ -1,18 +1,36 @@
 export class MenuComponent extends HTMLElement {
     async connectedCallback() {
+        this.clickEvents = [];
+
         this.innerHTML = `
             <div class='toggle' onclick="this.parentNode.classList.toggle('minimize')">
-                <div></div>
-                <div></div>
-                <div></div>
+            <span class="material-icons">menu</span>
             </div>
-            <div class="menu-item"><span class="material-icons">home</span><div>Home</div></div>
-            <div class="menu-item"><span class="material-icons">account_circle</span><div>Account</div></div>
+            <div id="home" class="menu-item"><span class="material-icons">home</span><div>Home</div></div>
+            <div id="account" class="menu-item"><span class="material-icons">account_circle</span><div>Account</div></div>
         `;
+
+        document.querySelectorAll('.menu-item')
+                .forEach(i => {
+                    const evt = this.onSelected.bind(this, i);
+                    this.clickEvents.push({element: i, event: evt});
+                    i.addEventListener('click', evt);
+                });
+    }
+
+    onSelected(e) {
+        const selected = document.querySelector('.menu_selected');
+        
+        if(selected)
+            selected.classList.remove('menu_selected');
+
+        window.location.hash = e.id;
+        e.classList.add('menu_selected');
     }
 
     disconnectedCallback() {
-
+        this.clickEvents
+            .forEach(i => i.element.removeEventListener('click', i.event));
     }
 }
 
